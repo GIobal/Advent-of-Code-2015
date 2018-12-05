bd7c72096eb208ae7c52fa1af6fef7f3dfde6352
GIobal <45609012+GIobal@users.noreply.github.com>
2018-12-05 16:18:59 +0100

    Remove excessive period.

--- Day-01/part1.js
+++ Day-01/part1.js
@@ -4,5 +4,5 @@ fs.readFile('input.txt', (error, data) => {
     if (error) throw error
 
     let contents = data.toString()
-    console.log((contents.split('(').length - 1) - (contents.split(')').length - 1)) // 138.
+    console.log((contents.split('(').length - 1) - (contents.split(')').length - 1)) // 138
 })
